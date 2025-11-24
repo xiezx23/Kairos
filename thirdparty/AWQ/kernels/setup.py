ce1862c8caa9ca12a01090e5a5694a4f89146e19
xiezx23 <xiezx23@mail2.sysu.edu.cn>
2025-11-24 22:17:57 +0800

    Fix commit

--- thirdparty/AWQ/kernels/setup.py
+++ thirdparty/AWQ/kernels/setup.py
@@ -41,11 +41,7 @@ setup(
                 "csrc/w8a8/act.cu",
                 "csrc/w8a8/layernorm.cu",
 
-<<<<<<< HEAD
                 "csrc/dequant_kairos/deq_i4_to_f16.cu"
-=======
-                "csrc/dequant_kairos/deq_i4_to_f16.cu"
->>>>>>> main
 
             ],
             extra_compile_args=extra_compile_args,
